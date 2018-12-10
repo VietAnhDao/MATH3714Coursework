51edeeb3b02e4027787b589d02e52d11ada94fac
VietAnhDao <viet.dao15@gmail.com>
2018-12-10 23:22:29 +0000

    Prediction left

--- Rcode.R
+++ Rcode.R
@@ -1,5 +1,7 @@
+#PACKAGE-USED---------------------------------------------------------------------------------------------------
+install.packages("GGally")
 #Power cut code
-#dat = read.table("~/Documents/Rcode/MATH3714/auto.csv", header = T, stringsAsFactors = F, sep = ",")
+#dat = read.table("~/Documents/Rcode/MATH3714/auto.csv", header = T,row.names = 1, stringsAsFactors = F, sep = ",")
 #dat$int = NULL
 #ERROR-CORRECTING-----------------------------------------------------------------------------------------------
 dat = read.table("http://www1.maths.leeds.ac.uk/~charles/math3714/Auto.csv", header = T, stringsAsFactors = F)
@@ -51,24 +53,24 @@ for(string in dat$make){
 dat$name=NULL
 
 #---Problem 4
+i=1
+for(string in unique(dat$make)){
+  print(paste("changing",string,"to",as.character(i), sep = " "))
+  for(make in dat$make){
+    if(make == string){
+      dat$make[dat$make==string]= as.integer(i)
+    }
+  }
+  i= i + 1
+}
 #changing categorical data to factor
 dat$make=factor(dat$make)
-# i=1
-# for(string in unique(dat$make)){
-#   print(paste("changing",string,"to",as.character(i), sep = " "))
-#   for(make in dat$make){
-#     if(make == string){
-#       dat$make[dat$make==string]= as.integer(i)
-#     }
-#   }
-#   i= i + 1
-# }
 
 #change origin to factors
-dat$origin[dat$origin==1]="American"
-dat$origin[dat$origin==2]="European"
-dat$origin[dat$origin==3]="Japanese"
-is.factor(dat$origin)
+# dat$origin[dat$origin==1]="American"
+# dat$origin[dat$origin==2]="European"
+# dat$origin[dat$origin==3]="Japanese"
+# is.factor(dat$origin)
 
 #change origin into factor
 dat$origin = factor(dat$origin)
@@ -95,7 +97,7 @@ sqrt_model = function(dat){
 
 #logarithmic model
 log_model = function(dat){
-  return(lm(log(mpg)~., data = dat))
+  return(lm(log(mpg) ~ ., data = dat))
 }
 
 recip_y = function(dat){
@@ -111,8 +113,17 @@ model2 = function(dat){
 }
 
 ridge=function(dat){
-  library(MASS)
-  return(lm.ridge(mpg~.,data = dat, lambda = seq(0, 0.000001, 0.0000001)))
+  #install.packages("ridge")
+  library(ridge)
+  return(linearRidge(formula = mpg ~ ., data = dat))
+}
+
+box_cox = function(dat){
+  base = lm(mpg~., data = dat)
+  boxcox.lm = boxcox(base)
+  return(boxcox.lm)
+  ## now consider more general transformations:
+  
 }
 #DIAGNOSTICS----------------------------------------------------------------------------------------------------
 Res_VS_value=function(model_no,model,dat){
@@ -137,6 +148,29 @@ Res_VS_value=function(model_no,model,dat){
   }
 }
 
+Res_VS_value1=function(model_no,model,dat){
+  #saving plot
+  png(paste(model_no,"res_vs_value.png",sep = "_"))
+  #Residual vs variables not including the make of the car
+  no_variable = 3
+  res = model$residuals
+  dattemp=as.data.frame(cbind(dat$prime, dat$acceleration, dat$year,dat$origin))
+  View(dattemp)
+  colnames(dattemp)=c("prime","acceleration","year")
+  par(mfrow=c(3,3))
+  for(i in 1:no_variable){
+    print(names(dattemp)[i])
+    plot(dattemp[,i],res,ylab = "residual",xlab = toString(names(dattemp)[i]))
+  }
+  dev.off()
+  
+  #plotting the plot
+  for(i in 1:no_variable){
+    print(names(dattemp)[i])
+    plot(dattemp[,i],res,ylab = "residual",xlab = toString(names(dattemp)[i]))
+  }
+}
+
 Res_VS_fit=function(model_no,model){
   #save the plot
   png(paste(model_no,"res_vs_fitted.png",sep = "_"))
@@ -144,7 +178,7 @@ Res_VS_fit=function(model_no,model){
   dev.off()
   
   #plot the plots
-  plot(preds,res,xlab = "fitted",ylab = "residuals")
+  plot(model,1)
   
 }
 
@@ -172,42 +206,199 @@ HomoSce=function(model_no,model){
 }
 
 check_assumption=function(model_no,model,dat){
+  print("Variance")
   HomoSce(model_no,model)
+  print("Q-Q")
   Normal(model_no,model)
+  print("Res VS value")
   Res_VS_value(model_no,model, dat)
+  print("Res VS fitted")
   Res_VS_fit(model_no,model)
 }
 
+check_assumption1=function(model_no,model,dat){
+  print("Variance")
+  HomoSce(model_no,model)
+  print("Q-Q")
+  Normal(model_no,model)
+  print("Res VS fitted")
+  Res_VS_fit(model_no,model)
+  print("Res VS value")
+  Res_VS_value1(model_no,model,dat)
+}
 
 check_multicollinearity=function(dat){
   #Check for none factors only
-  X=as.matrix(cbind(dat$cylinders,dat$displacement,dat$horsepower,dat$weight,dat$acceleration,dat$year))
+  X=as.matrix(dat)
   round(diag(solve(t(X)%*%X)),3) 
   v=eigen(t(X)%*%X)
-  round(v$values,1)
-  round(max(v$values)/v$values,0)
-  round(v$vectors,1)
+  print("Eigenvalues")
+  print(round(v$values,1))
+  print("Max Eigenvalue/Eigenvalues")
+  print(round(max(v$values)/v$values,0))
+  print("Eigenvectors")
+  print(round(v$vectors,1))
   S=svd(X)
-  S$d
-  max(S$d)/S$d
+  print(S$d)
+  print(max(S$d)/S$d)
 }
 
+pairwise_cor=function(dat){
+  library(GGally)
+  ggpairs(dat)
+}
 #BENCHMARK------------------------------------------------------------------------------------------------------
-bench_mark=function(dat){
+#use to predict value from a given dataset
+# predict = function(model,testData){
+#   predicted <- predict(model, testData)
+#   compare <- cbind(actual=testData$response, predicted)
+#   return(mean(apply(compare, 1, min)/apply(compare, 1, max)))
+# }
+# 
+# #Create model and plit data into training and testing data. 
+# #Then fit the model to the training data set
+# #Then pass it to test how well it predict against the testing data which the model haven't seen.
+# bench_mark=function(inputData){
+#   library(glmnet)
+#   trainingIndex <- sample(1:nrow(inputData), 0.8*nrow(inputData)) # indices for 80% training data
+#   trainingData <- inputData[trainingIndex, ] # training data
+#   testData <- inputData[-trainingIndex, ] # test data
+#   #print(trainingData)
+#   #print(testData)
+#   
+#   
+#   #MODEL  
+#   base = base_model(trainingData)
+#   square_root = sqrt_model(trainingData)
+#   Log_model = log_model(trainingData)
+#   rec_y = recip_y(trainingData)
+#   model1 = model2(trainingData)
+#   log_recip_model = lm(log(1/mpg)~., data = trainingData)
+#   #LinRidge = ridge(trainingData)
+#   
+#   #TESTING HOW GOOD PREDICTIONS ARE.
+#   predicted = cbind(predict(base, testData),predict(square_root, testData),predict(Log_model, testData),predict(rec_y, testData),
+#                     predict(model1, testData),predict(log_recip_model, testData),predict(LinRidge, testData))
+#   colnames(predicted) = c("base","sqrt","log","reciprocal","model1","log-reciprocal","Ridge")
+#   print(predicted)
+# }
+
+#Testing for the residual of the model
+res_bench=function(dat){
+  base = base_model(dat)
+  square_root = sqrt_model(dat)
+  Log_model = log_model(dat)
+  rec_y = recip_y(dat)
+  model1 = model2(dat)
+  log_recip_model = lm(log(1/mpg)~., data = dat)
+  LinRidge = ridge(dat)
+  res_summary=cbind(sum(base$residuals^2), sum(square_root$residuals^2),sum(Log_model$residuals^2),
+                    sum(rec_y$residuals^2),sum(model1$residuals^2),sum(log_recip_model$residuals^2),33.53)
+  colnames(res_summary)=c("base","square root","log","reciprocal","model1", "log reciprocal","Ridge")
+  print(res_summary)
 }
 
 #Diagnostics-of-Model-------------------------------------------------------------------------------------------
-#Running these codes will force save the plots onto the disk.
+
+#Initializing the models. These model uses the data that have multicollinearity involve.
 base = base_model(dat)
 square_root = sqrt_model(dat)
 Log_model = log_model(dat)
 rec_y = recip_y(dat)
 model1 = model2(dat)
 log_recip_model = lm(log(1/mpg)~., data = dat)
+LinRidge = ridge(dat)
 
+#Checking the assumption of models.
 check_assumption("base",base,dat)
 check_assumption("model1",model1,dat)
 check_assumption("sqrt",square_root,dat)
-check_assumption("log",log_model,dat)
+check_assumption("log",Log_model,dat)
 check_assumption("reciprocal",rec_y,dat)
 check_assumption("log_reciprocal",log_recip_model,dat)
+
+#Solving MultiCollinearity
+prime = (dat$cylinders+dat$weight)/(dat$horsepower+dat$displacement)
+check_multicollinearity(as.data.frame(cbind(prime,dat$acceleration,dat$year)))
+# [1] "Eigenvalues"
+# [1] 2423800.9    3567.0     688.2
+# [1] "Max Eigenvalue/Eigenvalues"
+# [1]    1  680 3522
+# [1] "Eigenvectors"
+# [,1] [,2] [,3]
+# [1,] -0.1 -0.5  0.9
+# [2,] -0.2 -0.8 -0.5
+# [3,] -1.0  0.2  0.0
+# [1] 1556.85611   59.72403   26.23276
+# [1]  1.00000 26.06750 59.34779
+
+vif(as.data.frame(cbind(prime,dat$acceleration,dat$year)))
+# Variables      VIF
+# 1     prime 2.103025
+# 2        V2 1.873807
+# 3        V3 1.175262
+
+
+#Data with Multicollinear solved.
+dat2=as.data.frame(cbind(dat$mpg,prime,dat$acceleration,dat$year,dat$origin,dat$make))
+colnames(dat2)=c("mpg","prime","acceleration","year","origin","make")
+base = base_model(dat2)
+square_root = sqrt_model(dat2)
+Log_model = log_model(dat2)
+rec_y = recip_y(dat2)
+log_recip_model = lm(log(1/mpg)~., data = dat2)
+LinRidge = ridge(dat2)
+
+#Checking the assumption of models.
+check_assumption1("base_cor",base,dat2)
+check_assumption1("model1_cor",model1,dat2)
+check_assumption1("sqrt_cor",square_root,dat2)
+check_assumption1("log_cor",Log_model,dat2)
+check_assumption1("reciprocal_cor",rec_y,dat2)
+check_assumption1("log_reciprocal_cor",log_recip_model,dat2)
+#IGNORE
+#Benchmarking the models. Unable to perform them due to R having limit on rams for stacks.
+# trainingIndex <- sample(1:nrow(dat), 0.99*nrow(dat))
+# trainingData <- dat[trainingIndex, ] 
+# testData <- dat[-trainingIndex, ]
+# base_test = base_model(trainingData)
+# square_root_test = sqrt_model(trainingData)
+# Log_model_test = log_model(trainingData)
+# rec_y = recip_y(trainingData)
+# model1 = model2(trainingData)
+# log_recip_model = lm(log(1/mpg)~., data = trainingData)
+# LinRidge = ridge(trainingData)
+# predicted <- predict(model, testData)
+# predicted = predict(base_test,testData)
+
+#Checking which model has the best residual
+res_bench(dat)
+
+#Checking-for-Outliers-----------------------------------------------------------------------------------------
+#Cook's distance
+check_cook=function(model_no,model,dat){
+  par(mfrow=c(1,2))
+  png(paste(model_no,"Cooks.png",sep = "_"),width = 1000, height = 1000)
+  plot(model,4)
+  plot(model,5)
+  dev.off()
+}
+
+#Logarithmic Model
+check_cook("log_cor",Log_model,dat)
+
+#Reciprocal Model
+check_cook("reciprocal_cor",rec_y,dat)
+
+summary(Log_model)
+summary(rec_y)
+
+#FINAL-TWO-MODEL------------------------------------------------------------------------------------------------
+dat3=dat2[-6]
+final_log = lm(log(mpg) ~ .^2, data = dat3)
+final_rec = lm(1/mpg ~ .^2, data = dat2[-6])
+
+step(final_log, direction="backward")
+step(final_rec,direction = "backward")
+
+#AIC------------------------------------------------------------------------------------------------------------
